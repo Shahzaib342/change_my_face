d22f7dc75e155249777db9ba718cd5d7a9c264c7
shahzaib ali <shahzaibalihassan@gmail.com>
2019-06-22 16:24:13 +0500

    adding new files and changing file structure of api

--- index.php
+++ index.php
@@ -1,103 +1,51 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Page Title</title>
-    <style>
-        .change-my-face-content{
-            width:800px;
-            margin: auto;
-        }
-        .change-my-face-content div{
-            width: 400px;
-            margin: auto;
-            margin-top: 50px;
-        }
-        .processed-data {
-            display: none;
-        }
-    </style>
-    <script
-            src="https://code.jquery.com/jquery-3.4.1.min.js"
-            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-            crossorigin="anonymous"></script>
+
+    <script src="jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Change My Face</title>
+
 </head>
 <body>
 <div>
     <div class="change-my-face-content">
         <div>
-        <h2>See how your lifestyle affects <br>your looks in years to come</h2>
+            <h2>See how your lifestyle affects <br>your looks in years to come</h2>
             <div class="change-my-face-img">
-            <img src="profile.jpg" alt="Upload Image">
-            <form action="abc.php" method="post" enctype="multipart/form-data" id="form">
-                Upload a photograph of yourself facing the camera.<br><br>
-                <input type="file" name="image" id="image">
-                <input type="text" name="action" id="action" value="upload_image" hidden>
-                <input type="text" name="api_key" id="api_key" value="t76zM9aQzI2TcvoXas4rGiggwpYpmOSq" hidden>
-                <input type="submit" value="Upload Image" name="submit">
-            </form>
+                <img src="profile.jpg" alt="Upload Image">
+                <form action="abc.php" method="post" enctype="multipart/form-data" id="form">
+                    Upload a photograph of yourself facing the camera.<br><br>
+                    <input type="file" name="image" id="image">
+                    <input type="text" name="action" id="action" value="upload_image" hidden>
+                    <input type="text" name="api_key" id="api_key" value="t76zM9aQzI2TcvoXas4rGiggwpYpmOSq" hidden>
+                    <input type="submit" value="Upload Image" name="submit">
+                    <div class="loader"></div>
+                </form>
             </div>
         </div>
         <div class="processed-data">
             <img src="profile.jpg" alt="Upload Image">
+            <div class="effects-variation">
+                <div>
+                    <a id="smoking" class="btn active" onclick="changeEffect(65,'smoking')">Smoking</a>
+                    <a id="drinking" class="btn" onclick="changeEffect(67,'drinking')">Drinking</a>
+                    <a id="aon-poor" class="btn" onclick="changeEffect(101,'aon-poor')">Aon (Poor)</a>
+                </div>
+                <div>
+                    <a id="stress" class="btn" onclick="changeEffect(75,'stress')">Stress</a>
+                    <a id="diet" class="btn" onclick="changeEffect(73,'diet')">Diet</a>
+                    <a id="aon-average" class="btn" onclick="changeEffect(100,'aon-average')">AON (Average)</a>
+                </div>
+                <div>
+                    <a id="aon-excellent" class="btn" onclick="changeEffect(99,'aon-excellent')">AON (Excellent)</a>
+                </div>
+            </div>
         </div>
     </div>
-<div>
+    <div>
 </body>
 </html>
-<script>
-$(document).ready(function (e) {
-$("#form").on('submit',(function(e) {
-e.preventDefault();
-$.ajax({
-url: "https://api.cmfapp.co.uk/api2/",
-type: "POST",
-data:  new FormData(this),
-contentType: false,
-cache: false,
-processData:false,
-beforeSend : function()
-{
-//$("#preview").fadeOut();
-$("#err").fadeOut();
-},
-success: function(data)
-{
-console.log(data);
-    var request = $.ajax({
-        url: "https://api.cmfapp.co.uk/api2/",
-        method: "POST",
-        data: {
-            action : 'apply_effects',
-            api_key:'t76zM9aQzI2TcvoXas4rGiggwpYpmOSq',
-            session_id : data.session_id,
-            'effects[effect_id]': 65
-
-        },
-    });
-
-
-    request.done(function( response ) {
-        console.log(response.data.effect_results[0].output_file);
-        $('.change-my-face-img').hide();
-        $('.processed-data img').attr('src',response.data.effect_results[0].output_file);
-        $('.processed-data').show();
-
-    });
-
-    request.fail(function( jqXHR, textStatus ) {
-        alert( "Request failed: " + textStatus );
-    });
-
-},
-error: function(e)
-{
-$("#err").html(e).fadeIn();
-}
-});
-}));
-});
-
-
-</script>
 
 
---
+++ script.js
@@ -0,0 +1,84 @@
+/**
+ * Created By Shahzaib 18/06/2019
+ **/
+
+var face = {
+    session_id: 0
+};
+
+$(document).ready(function (e) {
+
+    $("#form").on('submit', (function (e) {
+        $('.loader').show();
+        e.preventDefault();
+        $.ajax({
+            url: "https://api.cmfapp.co.uk/api2/",
+            type: "POST",
+            data: new FormData(this),
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function (data) {
+                face.session_id = data.session_id;
+                var request = $.ajax({
+                    url: "https://api.cmfapp.co.uk/api2/",
+                    method: "POST",
+                    data: {
+                        action: 'apply_effects',
+                        api_key: 't76zM9aQzI2TcvoXas4rGiggwpYpmOSq',
+                        session_id: data.session_id,
+                        'effects[effect_id]': 65
+
+                    },
+                });
+
+
+                request.done(function (response) {
+                    $('.loader').hide();
+                    $('.change-my-face-img').hide();
+                    $('.processed-data img').attr('src', response.data.effect_results[3].output_file);
+                    $('.processed-data').show();
+
+                });
+
+                request.fail(function (jqXHR, textStatus) {
+                    alert("Request failed: " + 'Please upload clear image with your face visible');
+                });
+
+            },
+            error: function (e) {
+                //
+            }
+        });
+    }));
+});
+
+function changeEffect(effect_id, id) {
+
+    var that = $(this);
+    var request = $.ajax({
+        url: "https://api.cmfapp.co.uk/api2/",
+        method: "POST",
+        data: {
+            action: 'apply_effects',
+            api_key: 't76zM9aQzI2TcvoXas4rGiggwpYpmOSq',
+            session_id: face.session_id,
+            'effects[effect_id]': effect_id
+
+        },
+    });
+
+
+    request.done(function (response) {
+        $('.btn.active').removeClass('active');
+        $('#' + id).addClass('active');
+        $('.change-my-face-img').hide();
+        $('.processed-data img').attr('src', response.data.effect_results[4].output_file);
+        $('.processed-data').show();
+
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+        alert("Request failed: " + 'Please try again');
+    });
+}
